35c815248b18b17983f6627db271b87c62e7ae56
Anisa Patra <anipatra2000@gmail.com>
2021-08-16 20:16:31 +0530

    Seventh COmmit

--- src/test/java/PrimeFactorTest.java
+++ src/test/java/PrimeFactorTest.java
@@ -1,8 +1,17 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PrimeFactorTest {
+
+    private List<Integer> list(int... ints) {
+        List<Integer> list = new ArrayList<>();
+        for (int i : ints)
+            list.add(i);
+        return list;
+    }
+
     @Test
     void testFactorf1() {
         PrimeFactor primefacts = new PrimeFactor();
@@ -19,5 +28,11 @@ public class PrimeFactorTest {
     void testFactorsof3() {
         PrimeFactor primefacts = new PrimeFactor();
         assertEquals(3,primefacts.prime(3),"Should return prime factors of given number.");
+    }
+
+    @Test
+    void testFactorsof4() {
+        PrimeFactor primefacts = new PrimeFactor();
+        assertEquals(list(2,2),primefacts.prime(4),"Should return prime factors of given number.");
     }
 }
