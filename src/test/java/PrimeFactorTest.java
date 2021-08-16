1b364c535b530418385f6941bcddb88a5aa494e1
Anisa Patra <anipatra2000@gmail.com>
2021-08-16 19:28:04 +0530

    Third Commit

--- src/test/java/PrimeFactorTest.java
+++ src/test/java/PrimeFactorTest.java
@@ -4,9 +4,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PrimeFactorTest {
     @Test
-    void testPrime() {
+    void testPrimeof1() {
         PrimeFactor primefacts = new PrimeFactor();
         assertEquals(1,primefacts.prime(1),"Should return prime factors of given number.");
     }
+
+    @Test
+    void testPrimeof2() {
+        PrimeFactor primefacts = new PrimeFactor();
+        assertEquals(2,primefacts.prime(2),"Should return prime factors of given number.");
+
+    }
+
 
 }
